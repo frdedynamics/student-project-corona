cea8738bf8a802d4bce87d798f769ecb457f6776
haggbart <mr.haggbart@gmail.com>
2020-03-27 18:27:09 +0100

    test

--- test/test_SEIR.py
+++ test/test_SEIR.py
@@ -1,5 +1,4 @@
 from unittest import TestCase
-from .context import src
 
 
 class TestSEIR(TestCase):
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from .context import src
+import src
 import app
 
 

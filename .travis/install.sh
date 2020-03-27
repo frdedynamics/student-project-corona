8dca016bd7237be816360de3b2485cdaa4186755
Johannes Møgster <johannes.mogster@mojotech.no>
2020-03-27 20:30:18 +0100

    let`s go venv!

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,9 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
     # Install some custom requirements on macOS
     # e.g. brew install pyenv-virtualenv
+    brew install pyenv-virtualenv
+    pyenv-virtualenv venv
+    source venv/bin/activate
 
     case "${TOXENV}" in
         py37)

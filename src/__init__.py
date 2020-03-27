65212ae20fdd315380d7c5f200312c1ed6913aec
Johannes Møgster <johannes.mogster@mojotech.no>
2020-03-27 18:01:34 +0100

    more fake news

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@
 from .config import get_config
 from .models import SIR
 from .models import SEIR
+from .models import SIR2
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 from .SIR import SIR
 from .SEIR import SEIR
+from .some_model import SIR2

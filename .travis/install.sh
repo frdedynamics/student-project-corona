f286793a7c8b6e9ffe17e7d2ac6560ca2f531759
Johannes Møgster <johannes.mogster@mojotech.no>
2020-03-28 15:14:50 +0100

    what`s in here?

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,7 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
     # Install some custom requirements on macOS
     # e.g. brew install pyenv-virtualenv
-    brew install python
-    python3-V
+    python3 -V
     pip3 -V
     pip3 install -r ./requirements.txt
 

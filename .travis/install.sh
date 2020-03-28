13b10fd4b4ad240b25f3b51e2e7bbb7ee211c6cb
Johannes Møgster <johannes.mogster@mojotech.no>
2020-03-28 16:42:44 +0100

    try windows

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $TRAVIS_OS_NAME = 'osx' ]; then
+if [ $TRAVIS_OS_NAME = 'osx' ] || [ $TRAVIS_OS_NAME = 'windows' ]; then
 
     # Install some custom requirements on macOS
     # e.g. brew install pyenv-virtualenv
@@ -8,14 +8,6 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
     pip3 -V
     pip3 install -r ./requirements.txt
 
-    case "${TOXENV}" in
-        py37)
-            # Install some custom Python 3.7 requirements on macOS
-            ;;
-        py38)
-            # Install some custom Python 3.8 requirements on macOS
-            ;;
-    esac
 else
     # Install some custom requirements on Linux
     pip install -r ./requirements.txt

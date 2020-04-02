584bcdee9ed9ca8e9d18f3fb32e6b956b3147151
haggbart <mr.haggbart@gmail.com>
2020-04-03 01:43:36 +0200

    get_json method and some refactoring

--- src/models/SEIR.py
+++ src/models/SEIR.py
@@ -2,6 +2,7 @@ import logging
 import numpy as np
 import matplotlib.pyplot as plt
 from src.config import get_config
+import json
 
 config = get_config()
 _LOGGER = logging.getLogger(__name__)
@@ -28,8 +29,8 @@ class SEIR:
         self.rho = rho  # social distancing (values: 0-1)
 
         self.t = np.linspace(0, self.duration_days, int(self.duration_days / timestep_days) + 1)
-        self.result_base_model = None
-        self.result_with_social_distancing = None
+        self.result = [None] * 2  # [0] = base model, [1] = social distancing
+        self.json = [None] * 2
         self.SEIR = plt
         self.plot_config()
 
@@ -38,11 +39,12 @@ class SEIR:
         S, E, I, R = [self.S_0], [self.E_0], [self.I_0], [self.R_0]
         dt = self.t[1] - self.t[0]
         rho = self.rho
+        result_index = 1
         if not social_distancing:
             rho = 1
+            result_index = 0
 
         for _ in self.t[1:]:
-
             next_S = S[-1] - (rho * self.beta * S[-1] * I[-1]) * dt
             next_E = E[-1] + (rho * self.beta * S[-1] * I[-1] - self.alpha * E[-1]) * dt
             next_I = I[-1] + (self.alpha * E[-1] - self.gamma * I[-1]) * dt
@@ -51,24 +53,24 @@ class SEIR:
             E.append(next_E)
             I.append(next_I)
             R.append(next_R)
+            self.result[result_index] = np.array([S, E, I, R]).T
 
-        if social_distancing:
-            self.result_with_social_distancing = np.stack([S, E, I, R]).T
-        else:
-            self.result_base_model = np.stack([S, E, I, R]).T
-
-    def __sanity_check_inputs(self):
-        if not False:
-            _LOGGER.error("Inputs are insane!")
-            raise ValueError
+    def get_json(self, social_distancing):
+        _LOGGER.debug("Json..")
+        index = 1 if social_distancing else 0
+        if self.result[index] is None:
+            self.solve(social_distancing)
+        dictionary = dict(zip(self.t, self.result[index].tolist()))
+        # print(json.dumps(dictionary, indent=4))
+        return json.dumps(dictionary, indent=4)
 
     def plot_base_model(self, draw=True):
-        if self.result_base_model is None:
+        if self.result[0] is None:
             self.solve(social_distancing=False)
 
         _LOGGER.debug("Plot base model result..")
 
-        self.SEIR.plot(self.t, self.result_base_model[:, 2] * self.total_population, label="Without social distancing")
+        self.SEIR.plot(self.t, self.result[0][:, 2] * self.total_population, label="Without social distancing")
 
         if draw:
             self.SEIR.legend(loc="upper right")
@@ -78,13 +80,13 @@ class SEIR:
                 self.SEIR.show(block=True)
 
     def plot_with_social_distancing(self):
-        if self.result_with_social_distancing is None:
+        if self.result[1] is None:
             self.solve()
 
         _LOGGER.debug("Plot with social distancing result..")
         self.plot_base_model(draw=False)
 
-        self.SEIR.plot(self.t, self.result_with_social_distancing[:, 2] * self.total_population, ls="--",
+        self.SEIR.plot(self.t, self.result[1][:, 2] * self.total_population, ls="--",
                        label="With social distancing (ρ = %.1f)" % self.rho)
 
         self.SEIR.legend(loc="upper right")

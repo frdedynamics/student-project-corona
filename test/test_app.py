89c66efc5cb5b136d53d6597804a9fc2665b7bfe
Johannes Møgster <johannes.mogster@mojotech.no>
2020-04-07 15:59:03 +0200

    paths yo, pathsetic!

--- test/test_app.py
+++ test/test_app.py
@@ -6,10 +6,10 @@ import app
 class TestApp(TestCase):
 
     def setUp(self):
-        with open('test_get_sir_data.json') as json_file:
+        with open('test/test_get_sir_data.json') as json_file:
             self.param = json.load(json_file)
 
-        with open('test_result_get_sir_data.json') as json_file:
+        with open('test/test_result_get_sir_data.json') as json_file:
             self.expected_result = json.load(json_file)
 
     def test_get_sir_data(self):
